1a47748d1115f569e8f70ac6315c4038f357bc63
campbell88 <shatharcampbell@gmail.com>
2017-04-13 15:35:25 -0400

    lab with extended challenges

---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Delcaring variables.
+            int dice;
+            int dice2;
+            char yes;
+            int numSides;
+
+            //Do-while loop to process the game.
+            do
+            {
+              
+                Console.WriteLine("Enter the number of sides for a pair of dice:");
+                // numSides = int.Parse(Console.ReadLine());
+                numSides = GetValidInteger();
+                Console.WriteLine("Welcome to the Grand Circus Casino!  Roll the dice? (y/n):");
+                //yes = char.Parse(Console.ReadLine());
+                yes = GetValidChar();
+
+                if (yes == 'y')
+                {
+                    Random rnd = new Random();
+
+                     dice = rnd.Next(1, numSides);
+                     dice2 = rnd.Next(1, numSides);
+
+                    Console.WriteLine(dice);
+                    Console.WriteLine(dice2);
+
+                        if (dice == 1 && dice2 == 1)
+                    {
+                        Console.WriteLine("You got snake eyes!" );
+                    }
+                        else if (dice == 6 && dice2 == 6)
+                    {
+                        Console.WriteLine("You got box cars!");
+                    }
+                        else if (dice + dice2 == 7 || dice + dice2 == 11)
+                    {
+                        Console.WriteLine("You got craps!");
+                    }
+
+                    Console.WriteLine("Thanks for playing!");
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("Thanks for playing!");
+                    return;
+                }
+
+            } while (true);
+
+        }
+
+
+
+
+        public static int GetValidInteger()
+        {
+            int input;
+           
+
+            while (!int.TryParse(Console.ReadLine(), out input))
+            {
+                Console.WriteLine("The input is not a number! Please enter a number! ");
+            }
+            return input;
+        }
+
+        public static char GetValidChar()
+        {
+            char Letter;
+
+            while (!char.TryParse(Console.ReadLine(), out Letter))
+            {
+                Console.WriteLine("The input is not 'y' or 'n'!");
+            }
+            return Letter;
+        }
+    }
+}
